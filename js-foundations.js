4ab862e3e43a7fbfb61db908400676fe5f798a9f
Ubuntu <ubuntu@ip-172-31-15-79.us-east-2.compute.internal>
2019-06-14 23:14:16 +0000

    assessment 2

--- js-foundations.js
+++ js-foundations.js
@@ -1,12 +1,58 @@
 // Javascript Foundations Assessments
 
 // 1. Write an anonymous function that takes one argument of type number and decides if that number is evenly divisble by three or not. If it is, print the number and "is divisible by three" or, print the number and "is not divisble by three".
+var x = function (a) {
+  if(a % 3 === 0){
+    return a + " is divisible by 3"
+  } else {
+    return a + " is not divisible by 3"
+  }
+}
+console.log(x(9))
+console.log(x(8))
 
 // 2. Write an object called helloMe. Include your first_name, last_name and at least two other properties of you. Write a function that returns a description of you.
+var helloMe = {
+  firstName: "Chris",
+  lastName: "McCarthy",
+  birthMonth: "September",
+  birthYear: 1991,
+  info: function () {
+    return this.firstName + " " + this.lastName + " was born in " + this.birthMonth + " " + this.birthYear;
+  }
+}
+
+console.log(helloMe.info())
 
 // 3. Create an array of 5 grocery items. Write a function that returns the first, middle, and last item as a string.
+var groceryList = ["apples", "spinach", "chicken", "eggs", "grapes"]
+
+function getIndexes (item) {
+  var length = item.length
+  var median = length / 2
+
+  if (length % 2 === 0) {
+    median = median
+  } else {
+    median = Math.floor(median)
+  }
+
+  console.log(`First item: ${item[0]}`)
+  console.log(`First item: ${item[median]}`)
+  console.log(`First item: ${item[length - 1]}`)
+}
+
+console.log(getIndexes(groceryList))
 
 // 4. Write a function called alphabetSoup that takes an argument of "str" and return a string with the letters in alphabetical order (ie. learn becomes aelnr). Assume numbers and punctuation symbols will not be included in the parameter.
+alphabetSoup = (str) => {
+    let newArr = str.toLowerCase().split("")
+    return newArr.sort((a,b) => {
+      return a>b? 1:-1
+    }).join("")
+  }
+
+console.log(alphabetSoup("learn"));
 
 // 5. Given the arrays below, write a function animalNums that uses a for loop to print one value from each array concatenated together.
 
@@ -14,6 +60,24 @@ var nums = [9, 5, 88, 2, 5, 42, 57]
 
 var nouns = ["ducks", "elephants", "pangolin", "rhinoceros", "giraffes", "penguins", "llamas"]
 
+animalNums = (arr1,arr2) => {
+  let newArr =[]
+
+  for (let i = 0; i <arr1 .length; i++) {
+    newArr[i] = `${arr1[i]}-${arr2[i]}`
+  }
+  return newArr
+}
+
+console.log(animalNums(nums,nouns));
+
 // output should be: "9 ducks", etc
 
 //6. Create an array of 5 numbers. Write a function that loops through the array and returns a new array of the numbers multiplied by 5.
+var num1 = [1,2,3,4,5]
+
+timesFive = (arr) => {
+  return arr.map(x => x*5)
+}
+
+console.log(timesFive(num1))
--- js-foundations.md
+++ js-foundations.md
@@ -3,24 +3,41 @@
 ### Without using Google answer the following:
 
 1. List the data types in Javascript. Indicate which are primitives.
+    There are 6 primitive data types: number, string, boolean, undefined, symbol, null.
+    Other data types include objects which are composed of behavior and data.
+
 
 2. What's the difference between =, ==, and === in JavaScript?
+    The single = sign is the assignment operator.
+    The double == sign is used as is equal to.
+    The triple === sign is used as is equal to and equal data type.
 
 3. What is the difference between an array's index and length?
+    The arrays index is always going to start with 0 and will be one less than the length.
+    The length will be equal to the number of values within the array.
 
 4. What are the three parts of a for loop?
+    The three parts of a for loop is the initializer list, the conditional check, and the post incrementer list.
 
 5. What is a function's declaration, argument, and call?
+    The function declaration is what defines the function with specific parameters.
+    The argument is the code written within the function.
+    The call is when you tell the function to run within your code.
 
 6. What are the three main steps in saving work to github?
+    The three main steps in saving work to github is git add ., git commit -m"", and git push
 
 7. What is the terminal command to move directories?
+    The terminal command to move directories is cd
 
 8. Do you have a suggestion for a Check In question?
+    If you could choose only one vacation destination what would it be?
 
 9. What was your favorite topic this week?
+    I really enjoyed learning the basics of react because it is such a powerful tool.
 
 10. What was your "A-ha!" moment this week?
+    My a-ha moment this week was doing the react assignment individually because it made me figure things out on my own.
 
 ### Stretch: The following questions are potential interview questions. First, try to answer each question on your own then Google the answer to further your knowledge.
 
@@ -35,7 +52,11 @@ function logIt(){
 
 logIt()
 ```
+It will return undefined because there are two declarations of the variable text.
 
 2. What is JSON? How does it relate to javascript objects?
+    JSON stands for Javascript object notation and is a syntax for storing and exchanging data.
 
 3. Describe a closure, what is it good for and how do you recognize one?
+    A closure is a function which remembers the environment in which it was created. 
+    You can recognize one when you see a function within a function.
